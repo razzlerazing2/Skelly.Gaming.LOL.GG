090db1707c08108a833cb396506d7fc7d6317a27
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-04-16 22:00:26 +0000

    awads

--- static/gg.js
+++ static/gg.js
@@ -23,8 +23,7 @@ function updateTime() {
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
-  const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
-  timeElement.textContent = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+  timeElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
 setInterval(updateTime, 10);
